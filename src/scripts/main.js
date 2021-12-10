97ea34ca6bbb7db558233a3f030e9cc9dfe9ec7d
nicholas-ewasiuk <ewasiuk.nick@gmail.com>
2021-12-09 17:26:31 -0800

    bouncing ball

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,33 @@
 const canvas = document.getElementById("game-window");
 const ctx = canvas.getContext("2d");
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+const dx = 2;
+const dy = -2;
 
-function draw() {
+const ballRadius = 10;
+
+function drawBall() {
   ctx.beginPath();
-  ctx.arc(50, 50, 10, 0, Math.PI*2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+
+  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+
+  if (y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
+  x += dx;
+  y += dy;
 
   requestAnimationFrame(draw);
 }
